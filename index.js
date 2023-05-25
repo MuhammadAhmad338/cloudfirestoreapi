5b2528e66a72d47a41c373eb0b3eefc4f17665c9
MuhammadAhmad338 <MuhammadAhmad338@gmail.com>
2023-05-25 19:49:50 +0500

    latest commit

--- index.js
+++ index.js
@@ -2,93 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const path = require('path');
 const Firestore = require("@google-cloud/firestore");
+const routes = require("./routes/routes");
 
 const app = express();
-const db = new Firestore({
-    projectId: 'famous-rhythm-362419',
-    keyFilename: path.join(__dirname, './creds.json')
-});
+
 
 app.use(express.json());
 // Enable CORS for all routes
 app.use(cors());
+
+app.use('/', routes);
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Barkbark rest api is listening at the: ${port}`);
 });
 
-app.get("/", async (req, res) => {
-    res.json({ status: 'Bark Bark ready to roll' });
-});
 
-app.get("/ahmad", async (req, res) => {
-    res.json({status: 'Ahmad hello world is the best app'});
-});
 
-app.get("/waleed", async (req, res) => {
-    res.json({status: 'Hello Waleed is the good boy!'});
-});
-
-app.get("/:breed", async (req, res) => {
-    const breed = req.params.breed;
-    const query = db.collection('dogs').where('name', '==', breed);
-    const querySnapshot = await query.get();
-
-    if (querySnapshot.size > 0) {
-        res.json(querySnapshot.docs[0].data());
-    } else {
-        res.json({ status: 'Not Found' });
-    }
-}); 
-
-app.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const query = db.collection('dogs').where('id', '==', id);
-    const querySnapshot = await query.get();
-    if (querySnapshot.size > 0) {
-        res.json(querySnapshot.docs[0].data());
-    } else {
-       res.json({status: 'Not Found!'});
-    }
-});
-
-app.post("/", async (req, res) => {
-    const data = {
-        id: req.body.id,
-        lifeExpectancy: req.body.lifeExpectancy,
-        name: req.body.name,
-        origin: req.body.origin,
-        type: req.body.type
-    }
-    await db.collection('dogs').doc(`${req.body.id}`).set(data);
-    res.json({ status: 'success', data: { dog: data } });
-});
-
-app.put('/edit/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = {
-            lifeExpectancy: req.body.lifeExpectancy,
-            name: req.body.name,
-            origin: req.body.origin,
-            type: req.body.type
-        }
-        await db.collection('dogs').doc(id).update(data);
-
-        res.json({status: 'Success updated', data: {updateDog: data}});
-    } catch(error) {
-      res.json({status: `Some error occured ${error}`});
-    }
-});
-
-app.delete('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        await db.collection('dogs').doc(id).delete();
-        res.json({status: "Got Deleted"});
-    } catch(error) {
-        res.json({status: `Some error occured ${error}`});
-    }
-});
 
---
+++ routes/routes.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const Firestore = require("@google-cloud/firestore");
+const path = require("path");
+
+const db = new Firestore({
+    projectId: 'famous-rhythm-362419',
+    keyFilename: path.join(__dirname, '../creds.json')
+});
+
+router.get("/", (req, res) => {
+    try {
+        res.json({status: "Bark Bark! api is listening!"})
+    } catch(e) {
+        res.json({status: "Some error occured!"});
+    }
+});
+
+router.get('/:breed', async (req, res) => {
+    const breed = req.params.breed;
+    const query = db.collection('dogs').where('name', '==', breed);
+    const querySnapshot = await query.get();
+
+    if (querySnapshot.size > 0) {
+        res.json(querySnapshot.docs[0].data());
+    } else {
+        res.json({ status: 'Not Found' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = db.collection('dogs').where('id', '==', id);
+    const querySnapshot = await query.get();
+    if (querySnapshot.size > 0) {
+        res.json(querySnapshot.docs[0].data());
+    } else {
+        res.json({status: 'Not Found!'});
+    }
+});
+
+router.post("/", async (req, res) => {
+    const data = {
+        id: req.body.id,
+        lifeExpectancy: req.body.lifeExpectancy,
+        name: req.body.name,
+        origin: req.body.origin,
+        type: req.body.type
+    }
+    await db.collection('dogs').doc(`${req.body.id}`).set(data);
+    res.json({ status: 'success', data: { dog: data } });
+});
+
+router.put('/edit/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = {
+            lifeExpectancy: req.body.lifeExpectancy,
+            name: req.body.name,
+            origin: req.body.origin,
+            type: req.body.type
+        }
+        await db.collection('dogs').doc(id).update(data);
+
+        res.json({status: 'Success updated', data: {updateDog: data}});
+    } catch(error) {
+        res.json({status: `Some error occured ${error}`});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await db.collection('dogs').doc(id).delete();
+        res.json({status: "Got Deleted"});
+    } catch(error) {
+        res.json({status: `Some error Occured ${error}`});
+    }
+});
+
+module.exports = router;
